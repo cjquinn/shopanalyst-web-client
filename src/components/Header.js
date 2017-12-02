1610935789103d343240667f794b626cbdcdbdfd
Christy Quinn <christyjquinn@gmail.com>
2017-12-02 17:17:24 +0000

    Fix to heading/nav padding

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,18 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 
+// Components
+import Template from './Template';
+
 const Header = ({ children }) => (
-    <div className="c-header u-1/1 u-baseline-x3 u-bgcolor-foreground u-fixed u-flex u-ai-center u-z">
-        <div className="o-container u-relative u-1/1">
-            {children}
+    <Template>
+        <div className="u-baseline-x3" />
+        <div className="c-header u-1/1 u-baseline-x3 u-bgcolor-foreground u-fixed u-flex u-ai-center u-z">
+            <div className="o-container u-relative u-1/1">
+                {children}
+            </div>
         </div>
-    </div>
+    </Template>
 );
 
 Header.propTypes = {
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 
 // Components
 import Svg from './Svg';
+import Template from './Template';
 
 // Spites
 import analyse from '../assets/svg/analyse.svg';
@@ -11,33 +12,36 @@ import settings from '../assets/svg/settings.svg';
 import shop from '../assets/svg/shop.svg';
 
 const Nav = ({ getClasses }) => (
-    <nav className="c-nav u-fixed u-1/1 u-z">
-        <div className="c-tabs o-type-small u-flex u-baseline-x6">
-            <Link
-                className={`c-tabs__tab u-1/3 u-color-inherit u-flex u-fd-col u-jc-between u-ai-center ${getClasses('lists')}`}
-                to="/lists"
-            >
-                <Svg className="u-1/1 u-baseline-x3" sprite={shop} />
-                <span>Shop</span>
-            </Link>
+    <Template>
+        <div className="u-baseline-x6" />
+        <nav className="c-nav u-fixed u-1/1 u-z">
+            <div className="c-tabs o-type-small u-flex u-baseline-x6">
+                <Link
+                    className={`c-tabs__tab u-1/3 u-color-inherit u-flex u-fd-col u-jc-between u-ai-center ${getClasses('lists')}`}
+                    to="/lists"
+                >
+                    <Svg className="u-1/1 u-baseline-x3" sprite={shop} />
+                    <span>Shop</span>
+                </Link>
 
-            <Link
-                className={`c-tabs__tab u-1/3 u-color-inherit u-flex u-fd-col u-jc-between u-ai-center ${getClasses('analyse')}`}
-                to="/analyse"
-            >
-                <Svg className="u-1/1 u-baseline-x3" sprite={analyse} />
-                <span>Analyse</span>
-            </Link>
+                <Link
+                    className={`c-tabs__tab u-1/3 u-color-inherit u-flex u-fd-col u-jc-between u-ai-center ${getClasses('analyse')}`}
+                    to="/analyse"
+                >
+                    <Svg className="u-1/1 u-baseline-x3" sprite={analyse} />
+                    <span>Analyse</span>
+                </Link>
 
-            <Link
-                className={`c-tabs__tab u-1/3 u-color-inherit u-flex u-fd-col u-jc-between u-ai-center ${getClasses('settings')}`}
-                to="/settings"
-            >
-                <Svg className="u-1/1 u-baseline-x3" sprite={settings} />
-                <span>Settings</span>
-            </Link>
-        </div>
-    </nav>
+                <Link
+                    className={`c-tabs__tab u-1/3 u-color-inherit u-flex u-fd-col u-jc-between u-ai-center ${getClasses('settings')}`}
+                    to="/settings"
+                >
+                    <Svg className="u-1/1 u-baseline-x3" sprite={settings} />
+                    <span>Settings</span>
+                </Link>
+            </div>
+        </nav>
+    </Template>
 );
 
 Nav.propTypes = {
